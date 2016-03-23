5f3afa449f18326e2c138ca12dab5f402565499a
Kerwin Money <kerwinso@rice.edu>
2016-03-23 09:58:14 -0500

    confirm server messaging after enqueue

--- batch-build-pdf/batch-build-pdf.py
+++ batch-build-pdf/batch-build-pdf.py
@@ -8,17 +8,19 @@ def chooseserver():
 
 # WORKS, but need to set the actual servers
 def servercheck():
-	global validserver, server, baseURL
+	global validserver, server, baseURL, servername
 	if server.lower() =='q':
 		exit()
 	elif server == '1':
 		validserver = True
 		print "Dev server chosen"
 		baseURL = 'http://legacy-textbook-dev.cnx.org/content/'
+		servername = 'Textbook-Dev'
 	elif server == '2':
 		validserver = True
 		print "QA server chosen"
 		baseURL = 'http://legacy-textbook-qa.cnx.org/content/'
+		servername = 'Textbook-QA'
 	else:
 		validserver = False
 		print "try again, enter 1 or 2"
@@ -44,10 +46,10 @@ for each in buildlist:
 	collID = each.strip()
 	if collID.startswith('col') and len(collID) == 8 and collID[3:].isdigit():
 		buildurl = baseURL + collID + "/latest/enqueue"
-		print "* " + collID + " enqueued"
+		print "* " + collID + " enqueued on " + servername
 	elif collID[0:4].isdigit() and len(collID) == 5:
 		buildurl = baseURL + "/col" + collID + "/latest/enqueue"
-		print "* " + collID + " enqueued"
+		print "* " + collID + " enqueued on " + servername
 	else: 
 		print "* Skipped collection " + collID + " due to incorrect formatting"
 		continue
