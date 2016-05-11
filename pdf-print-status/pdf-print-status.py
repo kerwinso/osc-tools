7bd68e623f2fd7af86780b833111d8cf5d83f2d5
Kerwin Money <kerwinso@rice.edu>
2016-05-11 15:47:43 -0500

    pps version 1.0

---
+++ pdf-print-status/pdf-print-status.py
@@ -0,0 +1,30 @@
+import urllib
+# Install BS4 first:
+# https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup
+from bs4 import BeautifulSoup
+
+inputfile = 'IDs.txt'
+idlist = open(inputfile)
+
+stmsgs = []
+
+for each in idlist:
+  collID = each.strip()
+  url = 'http://legacy.cnx.org/content/col'+collID+'/latest/printinfo'
+  html = urllib.urlopen(url).read()
+  soup = BeautifulSoup(html, "lxml")
+  for s in soup.find_all('div','status'):
+    span = s.find('span','data')
+    status = span.string
+    statusmsg = "Process status for col" + collID + ": " + status
+    print statusmsg
+    stmsgs.append(statusmsg)
+
+#print len(stmsgs)
+ 
+
+    # string match check
+    #if spanlist[7] == 'succeeded':
+    #  print "oK"
+    #else:
+    #  print "uh oh"
