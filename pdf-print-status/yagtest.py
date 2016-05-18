6769ff40173f3a18cfdaee46e16c6a04f9e40f79
Kerwin Money <kerwinso@rice.edu>
2016-05-18 16:49:49 -0500

    re-jiggered contents

--- pdf-print-status/yagtest.py
+++ pdf-print-status/yagtest.py
@@ -5,7 +5,7 @@ def ymail():
     yag = yagmail.SMTP('oscontentqa')
     vari = "some string here"
     contents = [
-    'This is the body, and here is just text. Next, here is a variable: ' + vari +'. \n This should be on a new line.'
+    'Body of the email text here. Next, here is a variable: ' + vari +'. \n This should be on a new line.'
     ]
     yag.send('brw5@rice.edu', 'Yagmail test', contents)
 
